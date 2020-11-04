4cc71728abc1642ee0d983854ef7e001b372b8c6
Gurin <gurinjaw@gmail.com>
2020-11-04 13:38:28 +0200

    Damage triggers functional

---
+++ Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public int playerNumber = 1;
+
+    private DamageTrigger[] damageTriggers = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        damageTriggers = GetComponentsInChildren<DamageTrigger>();
+        SubscribeToEvents();
+    }
+
+    void OnDestroy()
+    {
+        UnsubscribeFromEvents();
+    }
+
+    void SubscribeToEvents()
+    {
+        SubscribeToTriggerEvents();
+    }
+
+    void UnsubscribeFromEvents()
+    {
+        UnsubscribeFromTriggerEvents();
+    }
+
+    void SubscribeToTriggerEvents()
+    {
+        for (int i = 0; i < damageTriggers.Length; i++)
+        {
+            damageTriggers[i].OnTrigger += OnTrigger;
+        }
+    }
+
+    void UnsubscribeFromTriggerEvents()
+    {
+        for (int i = 0; i < damageTriggers.Length; i++)
+        {
+            damageTriggers[i].OnTrigger -= OnTrigger;
+        }
+    }
+
+    void OnTrigger(Collider other)
+    {
+        if (playerNumber == 1) return;
+
+        if (other.tag == "Player_1_Damage")
+            Debug.Log("Trigger enter! " + other.tag);
+    }
+}
---
+++ Assets/Scripts/Character/DamageTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTrigger : MonoBehaviour
+{
+    public delegate void TriggerEnterDelegate(Collider other);
+    public TriggerEnterDelegate OnTrigger = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (null != OnTrigger)
+            OnTrigger(other);
+    }
+}
